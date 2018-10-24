7121de81be878cfa62e293d0611436494f0dadb5
pandhe24 <40481573+pandhe24@users.noreply.github.com>
2018-10-24 21:30:18 +0700

    Initial commit

---
+++ app/src/main/java/com/landak/eplanning/MyConfig.java
@@ -0,0 +1,8 @@
+package com.landak.eplanning;
+
+public final class MyConfig {
+    public String main_url="http://ayampakusu.com/";
+static final int REQUEST_QR_CODE=1007;
+static final int REQUEST_PAY_WITH_VOUCHER=1008;
+static final int REQUEST_VOUCHER=1009;
+        }
