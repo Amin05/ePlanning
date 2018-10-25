f778712d4790baf2a80b87a75f818e4d70723661
M-aminPC <muhammadamin0598@gmail.com>
2018-10-25 21:50:00 +0700

    tambah

--- app/src/main/java/com/landak/eplanning/MainActivity.java
+++ app/src/main/java/com/landak/eplanning/MainActivity.java
@@ -1,13 +1,44 @@
 package com.landak.eplanning;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnTpUrusan, btnUrusan, btnBdUrusan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTpUrusan =findViewById(R.id.btnTpUrusan);
+        btnUrusan   =findViewById(R.id.btnUrusan);
+        btnBdUrusan =findViewById(R.id.btnBdUrusan);
+
+        btnTpUrusan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Memunculkan pesan berupa text dialog saat tombol "Klik Disini" diklik.
+                Toast.makeText(getApplicationContext(), "PILIH TIPE URUSAN", Toast.LENGTH_SHORT).show();
+
+                String[] singleChoiceItems = getResources().getStringArray(R.array.dialog_single_choice_array);
+                int itemSelected = 0;
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                builder1.setTitle("Select your gender")
+                        .setSingleChoiceItems(singleChoiceItems, itemSelected, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int selectedIndex) {
+
+                            }
+                        })
+                        .setPositiveButton("Ok", null)
+                        .setNegativeButton("Cancel", null)
+                        .show();
+            }
+        });
     }
 }
